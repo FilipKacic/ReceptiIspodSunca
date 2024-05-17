a00e4a7f0dc8918e5968fa63ccef3c11f6beade5
Filip Kačić <151817961+FilipKacic@users.noreply.github.com>
2024-05-17 18:33:28 +0200

    male promjene ideje

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,7 @@ from django.conf.urls.static import static
 app_name = 'main'
 
 urlpatterns = [
-    path('homepage', views.homepage, name='homepage'),
-    path('all_recipes', views.all_recipes, name='all_recipes'),
+    path('recipes_under_the_sun', views.all_recipes, name='all_recipes'),
     path('user_recipes', views.user_recipes, name='user_recipes'),
     path('edit_recipe/<int:recipe_id>/', user_edit_recipe, name='user_edit_recipe'),
     path('delete_recipe/<int:recipe_id>/', user_delete_recipe, name='user_delete_recipe'),
